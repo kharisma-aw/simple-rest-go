fbac6aa38489ce7563b570628d1061791a9b018d
kharisma-aw <aw.kharisma@gmail.com>
2020-02-11 17:26:50 +0700

    modify into restful

--- main.go
+++ main.go
@@ -6,9 +6,19 @@ import (
 )
 
 func apiResponse(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"message":"hello world!"}`))
+
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "GET method requested"}`))
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message": "POST method requested"}`))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message": "Can't find method requested"}`))
+	}
 }
 
 func main() {
